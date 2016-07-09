fdf1d59c01c6f38671a1bf934e267cbc62e2c703
Ze Vinicius <jvmirca@gmail.com>
2016-07-09 09:47:42 -0300

    add subtrac_psf; improve tests

--- photutils/psf/tests/test_nstar.py
+++ photutils/psf/tests/test_nstar.py
@@ -4,7 +4,7 @@ import numpy as np
 from astropy.table import Table
 from astropy.stats import gaussian_sigma_to_fwhm
 from astropy.modeling.fitting import LevMarLSQFitter
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_array_equal
 from ..models import IntegratedGaussianPRF
 from ...datasets import make_gaussian_sources
 from ...datasets import make_noise_image
@@ -23,7 +23,7 @@ class TestNStarPSFPhotometry(object):
         sigma_psf = 2.0
         sources = Table()
         sources['flux'] = [700, 700]
-        sources['x_mean'] = [12, 17]
+        sources['x_mean'] = [12, 18]
         sources['y_mean'] = [15, 15]
         sources['x_stddev'] = [sigma_psf, sigma_psf]
         sources['y_stddev'] = sources['x_stddev']
@@ -31,11 +31,12 @@ class TestNStarPSFPhotometry(object):
         tshape = (32, 32)
 
         image = (make_gaussian_sources(tshape, sources) +
-                 make_noise_image(tshape, type='poisson', mean=1.,
+                 make_noise_image(tshape, type='poisson', mean=4.,
                                   random_state=1)) 
 
         bkgrms = StdBackgroundRMS(sigma=3.)
         std = bkgrms(image)
+
         daofind = DAOStarFinder(threshold=5.0*std,
                                 fwhm=sigma_psf*gaussian_sigma_to_fwhm)
         daogroup = DAOGroup(1.5*sigma_psf*gaussian_sigma_to_fwhm)
@@ -52,32 +53,36 @@ class TestNStarPSFPhotometry(object):
         assert_allclose(result_tab['x_fit'], sources['x_mean'], rtol=1e-2)
         assert_allclose(result_tab['y_fit'], sources['y_mean'], rtol=1e-2)
         assert_allclose(result_tab['flux_fit'], sources['flux'], rtol=1e-1)
+        assert_array_equal(result_tab['id'], np.arange(2) + 1)
+        assert_array_equal(result_tab['group_id'], np.ones(2, dtype=np.int))
+        assert_allclose(np.mean(residual_image), 0.0, atol=1e-1)
 
     def test_complete_photometry_two(self):
         sigma_psf = 2.0
         sources = Table()
-        sources['flux'] = [700, 500, 500, 600]
+        sources['flux'] = [700, 700, 700, 700]
         sources['x_mean'] = [12, 18, 12, 18]
-        sources['y_mean'] = [15, 15, 20, 20]
+        sources['y_mean'] = [15, 15, 21, 21]
         sources['x_stddev'] = sigma_psf*np.ones(4)
         sources['y_stddev'] = sources['x_stddev']
         sources['theta'] = [0, 0, 0, 0]
         tshape = (32, 32)
 
         image = (make_gaussian_sources(tshape, sources) +
-                 make_noise_image(tshape, type='poisson', mean=1.,
+                 make_noise_image(tshape, type='poisson', mean=2.,
                                   random_state=1))
         
         bkgrms = StdBackgroundRMS(sigma=3.)
         std = bkgrms(image)
+
         daofind = DAOStarFinder(threshold=5.0*std,
                                 fwhm=sigma_psf*gaussian_sigma_to_fwhm)
         daogroup = DAOGroup(1.5*sigma_psf*gaussian_sigma_to_fwhm)
-        median = MedianBackground(sigma=3.)
+        median_bkg = MedianBackground(sigma=3.)
         psf_model = IntegratedGaussianPRF(sigma=sigma_psf)
         fitter = LevMarLSQFitter()
         nstar_photometry = NStarPSFPhotometry(find=daofind, group=daogroup,
-                                              bkg=median, psf=psf_model,
+                                              bkg=median_bkg, psf=psf_model,
                                               fitter=LevMarLSQFitter(),
                                               niters=1, fitshape=(5,5))
         
@@ -86,3 +91,6 @@ class TestNStarPSFPhotometry(object):
         assert_allclose(result_tab['x_fit'], sources['x_mean'], rtol=1e-2)
         assert_allclose(result_tab['y_fit'], sources['y_mean'], rtol=1e-2)
         assert_allclose(result_tab['flux_fit'], sources['flux'], rtol=1e-1)
+        assert_array_equal(result_tab['id'], np.arange(4) + 1)
+        assert_array_equal(result_tab['group_id'], np.ones(4, dtype=np.int))
+        assert_allclose(np.mean(residual_image), 0.0, atol=1e-1)
